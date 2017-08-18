1ae5195a58150632055165fc1771f41cbc999429
Carter Williamson <cwilliamson@luxoft.com>
2017-08-17 22:15:42 -0700

    Removed old code

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#API_KEY var must be set
-[ -z "$API_KEY" ] && { echo "Need to set API_KEY"; exit 1; }
-sed -i -e "s/API_KEY/${API_KEY}/g" ./webhook.json
 
 particle compile electron --saveTo firmware.bin
 
