9344fe4f46e6de48ef9fab5254b69b8abf09b77a
Carter Williamson <cwilliamson@luxoft.com>
2017-08-18 11:41:15 -0700

    Minor fixes

--- create-webhook.sh
+++ create-webhook.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #API_KEY var must be set
 API=""
-if [ -z "$API_KEY" ] then
-  if [ -z "$1" ] then
-    echo "Need to set API_KEY, or pass in your api key as an argument"; exit 1;
+if [ -z "$API_KEY" ]; then
+  if [ -z "$1" ]; then
+    echo "Need to set API_KEY, or pass in your api key as an argument"
+    exit 1
   else
     API="$1"
   fi
@@ -14,4 +15,5 @@ fi
 sed -i -e "s/API_KEY/${API_KEY}/g" ./webhook.json
 
 particle webhook create webhook.json
+rm webhook.json-e
 exit 0;
