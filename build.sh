117c4bc8f0187673cd78799bcabbfc0c57e42336
Carter Williamson <cwilliamson@luxoft.com>
2017-08-18 11:52:12 -0700

    Updated build script and gitignore

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-particle compile electron --saveTo firmware.bin
-
+particle compile electron ./src ./lib/google-maps-device-locator/src project.properties --saveTo firmware.bin
+if [[ $? -ne 0 ]]; then
+  echo "Failed to compile, exiting."
+  exit 1;
+fi
 particle flash --usb firmware.bin
+exit 0;
